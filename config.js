0708411ac41ab2e43f8ffab590927c4221c428a5
REEFHA <147322843+REEFHA@users.noreply.github.com>
2023-10-08 16:27:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'REEFHA', true],
+  ['0787644088', 'REEFHA', true],
   [''],
   [''] 
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
+global.mods = ['0787644088''] 
 global.prems = ['919074692450', '919074692450', '919074692450']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
